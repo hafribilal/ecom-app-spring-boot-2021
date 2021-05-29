a67c652ba8db16c851afae67a79480ea37d36114
Bilal Hafri <hafribilal@gmail.com>
2021-05-30 00:50:48 +0100

    Client Repository and Service modifications

--- src/main/java/org/cigma/ecom/repository/ClientRepository.java
+++ src/main/java/org/cigma/ecom/repository/ClientRepository.java
@@ -1,5 +1,6 @@
 package org.cigma.ecom.repository;
 
+import org.cigma.ecom.model.Administrateur;
 import org.cigma.ecom.model.Client;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -9,10 +10,10 @@ import java.util.List;
 
 public interface ClientRepository extends CrudRepository<Client,Integer>{
     @Query("select case when count(c)> 0 then true else false end from Client c where lower(c.username) like lower(:username)")
-    public boolean existsUsername(@Param("username") String username);
+    boolean existsUsername(@Param("username") String username);
 
     @Query("select case when count(c)> 0 then true else false end from Client c where lower(c.email) like lower(:email)")
-    public boolean existsEmail(@Param("email") String email);
+    boolean existsEmail(@Param("email") String email);
 
-    public Client selectClientByClientName(String name);
+    Administrateur findClientByUsername(String username);
 }
