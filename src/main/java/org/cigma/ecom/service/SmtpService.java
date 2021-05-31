1c570a1d2cdeb0de830a1b9e35f47f5bf044f2b0
Bilal Hafri <hafribilal@gmail.com>
2021-05-31 01:41:37 +0100

    SMTP Service changes

--- src/main/java/org/cigma/ecom/service/SmtpService.java
+++ src/main/java/org/cigma/ecom/service/SmtpService.java
@@ -1,17 +1,15 @@
 package org.cigma.ecom.service;
 
-import org.cigma.ecom.model.Client;
 import org.cigma.ecom.model.Compt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SmtpService {
-    private JavaMailSender javaMailSender;
+    private final JavaMailSender javaMailSender;
 
 
     @Autowired
@@ -23,7 +21,7 @@ public class SmtpService {
         //send mail
         SimpleMailMessage mail = new SimpleMailMessage();
         mail.setTo(compt.getEmail());
-        mail.setFrom("amine.youssef.0202@gmail.com");
+        mail.setFrom("cigma.d2j.2020@gmail.com");
         mail.setSubject("Spring mail notification test");
         mail.setText("TEST MESSAGE");
 
